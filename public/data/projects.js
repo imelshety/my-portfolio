412a2f5c7a96756013a8e810a41b9ca64092bc5e
unknown <elshetydev99@gmail.com>
2024-01-06 14:39:07 +0200

    edit url path maflam project

--- public/data/projects.js
+++ public/data/projects.js
@@ -1,84 +1,92 @@
 export const projects = [
-    {
-        id: 1,
-        image: '/assests/projects/maflam.png',
-        title: 'Mflam (freelance project)',
-        description: ' ReactJS Movie Web App using The Movie Database(TMDB) Open API',
-        type: 'react',
-        demo: 'https://imelshety.github.io/Movie-App-React/#/signin',
-        github: ' https://github.com/imelshety/Movie-App-React'
-    },
-    {
-        id: 2,
-        image: '/assests/projects/food-order.png',
-        title: 'Food Order App',
-        type: 'react',
-        description: ' through the course React - The Complete Guide (incl Hooks, React Router, Redux) on Udemy. state management of this app with reducer and context.',
-        demo: 'https://imelshety.github.io/Food-Order-app/',
-        github: 'https://github.com/imelshety/Food-Order-app'
-    },
-    {
-        id: 3,
-        image: '/assests/projects/moive-app.png',
-        title: 'Moives DB App',
-        description: 'Movie-app providers Deatiles of Trending Movies , Movie-app has a modern responsive UI ',
-        type: 'react',
-        demo: 'https://imelshety.github.io/Movie-App-React/#/signin',
-        github: ' https://github.com/imelshety/Movie-App-React'
-    },
-    {
-        id: 4,
-        image: '/assests/projects/daunku.png',
-        title: 'Daunku-LandingPage',
-        description: ' The project is a landing page , The Website has a Responsive UI.',
-        type: 'react',
-        demo: 'https://daunku-six.vercel.app/',
-        github: 'https://github.com/imelshety/Daunku-LandingPage-Reactjs'
-    },
-    {
-        id: 5,
-        image: '/assests/projects/NEXCENT.png',
-        title: 'NEXCENT-Clone',
-        description: "The Website has a Responsive UI. The project is a landing page",
-        type: 'react',
-        demo: 'https://landing-page-reactjs-orpin.vercel.app/',
-        github: 'https://github.com/imelshety/Landing-page-reactjs'
-    },
-    {
-        id: 6,
-        image: '/assests/projects/axista.png',
-        title: 'axista-clone ',
-        description: ' I have focused more on Tailwind CSS than other aspects of this project.',
-        type: 'react',
-        demo: 'https://axista-clone-r7ot-8a4jwh1ob-imelshety.vercel.app/',
-        github: 'https://github.com/imelshety/axista-clone'
-    },
-    {
-        id: 7,
-        image: '/assests/projects/weather.png',
-        title: 'React-Weather-Api ',
-        description: 'The Website displays the weather by the city that is entered via Api. The API From This site displays the weather by the city entered By Using Axios.',
-        type: "react",
-        demo: 'https://imelshety.github.io/Simple-React-Weather/',
-        github: 'https://github.com/imelshety/Simple-React-Weather'
-    },
-    {
-        id: 8,
-        image: '/assests/projects/daniel.png',
-        title: 'daniel-s-WebSite-Bootstrap',
-        description: "The Website has a Responsive UI. Html, Css, Bootstrap,Responsive Ui,MediaQueris.",
-        type: "js",
-        demo: 'https://imelshety.github.io/daniel-s-WebSite-Bootstrap/',
-        github: 'https://github.com/imelshety/daniel-s-WebSite-Bootstrap'
-    },
-    {
-        id: 9,
-        image: '/assests/projects/yummy-food.png',
-        title: 'yummy-food jQuery Project',
-        description: "yummy-food has Category, Area, Contact Us Pages. yummy-food Provides an Overveiw of individual Meal.",
-        type: "js",
-        demo: 'https://imelshety.github.io/yummy-food/',
-        github: ' https://github.com/MohamedElsehty/yummy-food'
-    },
-    
-]
+  {
+    id: 1,
+    image: "/assests/projects/maflam.png",
+    title: "Mflam (freelance project)",
+    description:
+      " ReactJS Movie Web App using The Movie Database(TMDB) Open API",
+    type: "react",
+    demo: "https://maflam.vercel.app/",
+    github: "https://github.com/imelshety",
+  },
+  {
+    id: 2,
+    image: "/assests/projects/food-order.png",
+    title: "Food Order App",
+    type: "react",
+    description:
+      " through the course React - The Complete Guide (incl Hooks, React Router, Redux) on Udemy. state management of this app with reducer and context.",
+    demo: "https://imelshety.github.io/Food-Order-app/",
+    github: "https://github.com/imelshety/Food-Order-app",
+  },
+  {
+    id: 3,
+    image: "/assests/projects/moive-app.png",
+    title: "Moives DB App",
+    description:
+      "Movie-app providers Deatiles of Trending Movies , Movie-app has a modern responsive UI ",
+    type: "react",
+    demo: "https://imelshety.github.io/Movie-App-React/#/signin",
+    github: " https://github.com/imelshety/Movie-App-React",
+  },
+  {
+    id: 4,
+    image: "/assests/projects/daunku.png",
+    title: "Daunku-LandingPage",
+    description:
+      " The project is a landing page , The Website has a Responsive UI.",
+    type: "react",
+    demo: "https://daunku-six.vercel.app/",
+    github: "https://github.com/imelshety/Daunku-LandingPage-Reactjs",
+  },
+  {
+    id: 5,
+    image: "/assests/projects/NEXCENT.png",
+    title: "NEXCENT-Clone",
+    description:
+      "The Website has a Responsive UI. The project is a landing page",
+    type: "react",
+    demo: "https://landing-page-reactjs-orpin.vercel.app/",
+    github: "https://github.com/imelshety/Landing-page-reactjs",
+  },
+  {
+    id: 6,
+    image: "/assests/projects/axista.png",
+    title: "axista-clone ",
+    description:
+      " I have focused more on Tailwind CSS than other aspects of this project.",
+    type: "react",
+    demo: "https://axista-clone-r7ot-8a4jwh1ob-imelshety.vercel.app/",
+    github: "https://github.com/imelshety/axista-clone",
+  },
+  {
+    id: 7,
+    image: "/assests/projects/weather.png",
+    title: "React-Weather-Api ",
+    description:
+      "The Website displays the weather by the city that is entered via Api. The API From This site displays the weather by the city entered By Using Axios.",
+    type: "react",
+    demo: "https://imelshety.github.io/Simple-React-Weather/",
+    github: "https://github.com/imelshety/Simple-React-Weather",
+  },
+  {
+    id: 8,
+    image: "/assests/projects/daniel.png",
+    title: "daniel-s-WebSite-Bootstrap",
+    description:
+      "The Website has a Responsive UI. Html, Css, Bootstrap,Responsive Ui,MediaQueris.",
+    type: "js",
+    demo: "https://imelshety.github.io/daniel-s-WebSite-Bootstrap/",
+    github: "https://github.com/imelshety/daniel-s-WebSite-Bootstrap",
+  },
+  {
+    id: 9,
+    image: "/assests/projects/yummy-food.png",
+    title: "yummy-food jQuery Project",
+    description:
+      "yummy-food has Category, Area, Contact Us Pages. yummy-food Provides an Overveiw of individual Meal.",
+    type: "js",
+    demo: "https://imelshety.github.io/yummy-food/",
+    github: " https://github.com/MohamedElsehty/yummy-food",
+  },
+];
